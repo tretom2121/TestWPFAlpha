5ee93807c2028e53814fe158c7f021911355e5cf
tt <t.trencsenyi@gmail.com>
2022-10-08 23:11:54 +0200

    Add project files.

---
+++ TestWPFAlpha/DocumentStructureWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPFAlpha
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DocumentStructureWindow : Window
+    {
+        public DocumentStructureWindow()
+        {
+            InitializeComponent();
+
+            var newWin = new Window1(this);
+            newWin.Show();  
+
+        }
+
+        private void buttonCloseDocumentStructureWindow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TestWPFAlpha/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPFAlpha
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private DocumentStructureWindow _documentStructureWindow;
+
+        public Window1(DocumentStructureWindow documentStructureWindow)
+        {
+            InitializeComponent();
+            _documentStructureWindow = documentStructureWindow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tds = _documentStructureWindow.TreeViewDocumentStructure;
+            tds.Items.Add(new TreeViewItem() { Header = DateTime.Now.ToString("yyMMdd_HHmmss_fff")} );
+        }
+    }
+}
